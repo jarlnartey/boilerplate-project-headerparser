6e622154705717e2e117471aec3ef0a4f0ed247b
Jarl Nartey <jarlnartey@gmail.com>
2022-08-28 11:44:39 +0200

    Attempt 1 FCC headerpaser

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+const { request } = require('http');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,7 +25,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.enable("trust proxy")
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    "ipadress": req.ip, "language": req.get("Accept-Language"), "software": req.get("User-Agent")
+  })
+})
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
